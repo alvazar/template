24049ddb0fb78b6614e0e3b022ce49fd1da84e5f
alvazar <alvazar@gmail.com>
2020-03-31 22:02:43 +0300

    update

--- template.js
+++ template.js
@@ -1,81 +1,86 @@
-class Template {
-	constructor() {
-	}
+class Template
+{
+    constructor()
+    {
+    }
 
-	_getBlockTemplate(name,template) {
-		const regexp = '<!--\\s?b\\['+name+'\\]\\s?\\{\\s?-->.+<!--\\s?\\}\\s?b\\['+name+'\\]\\s?-->';
-		let match = template.match(new RegExp(regexp,'gms'));
-		return match !== null ? match[0] : "";
-	}
-	
-	make(path,data,onlyBlock) {
-		return new Promise((resolve,reject) => {
-			$.get(path,template => {
-				let result = this.makeTemplate(template,data,onlyBlock);
-				resolve(result);
-			});
-		});
-	}
+    getBlockTemplate(name, template)
+    {
+        const regexp = '<!--\\sb\\[' + name + '\\]\\s\\{\\s-->.+<!--\\s\\}\\sb\\[' + name + '\\]\\s-->';
+        let match = template.match(new RegExp(regexp, 'gms'));
+        return match !== null ? match[0] : "";
+    }
+    
+    make(path, data, onlyBlock)
+    {
+        return new Promise((resolve, reject) => {
+            $.get(path, template => {
+                let result = this.makeTemplate(template, data, onlyBlock);
+                resolve(result);
+            });
+        });
+    }
 
-	makeTemplate(template,data,onlyBlock) {
-		if (onlyBlock !== undefined) {
-			template = this._getBlockTemplate(onlyBlock,template);
-		}
-		for (let name in data) {
-			let value = data[name];
-			template = this._blockReplace(name,value,template);
-		}
-		return this.skipTemplateTags(template);
-	}
-	
-	_blockReplace(name,data,template) {
-		let blockTemplate = this._getBlockTemplate(name,template);
-		let blockMaked = blockTemplate;
-		
-		// remove block
-		if (data === false) {
-			return template.replace(blockMaked,"");
-		}
-		
-		const isMultiple = data instanceof Array && data[0] !== undefined;
-		
-		// prepare
-		if (isMultiple) {
-			blockMaked = "";
-			for (let key in data) {
-				let value = data[key];
-				if (typeof(value) == "object") {
-					blockMaked += this._blockReplace(name,value,blockTemplate);
-				}
-			}
-		}
-		else {
-			for (let key in data) {
-				let value = data[key];
-				// prepare sub block
-				if (typeof(value) == "object" || value === false) {
-					blockMaked = this._blockReplace(key,value,blockMaked);
-				}
-				// replace mask on value
-				else {
-					blockMaked = this.replaceAll('<!-- v['+key+'] -->',value,blockMaked);
-				}
-			}
-		}
-		
-		// replace block
-		template = this.replaceAll(blockTemplate,blockMaked,template);
-		
-		return template;
-	}
+    makeTemplate(template, data, onlyBlock)
+    {
+        if (onlyBlock !== undefined) {
+            template = this.getBlockTemplate(onlyBlock, template);
+        }
+        for (let name in data) {
+            let value = data[name];
+            template = this.blockReplace(name, value, template);
+        }
+        return this.skipTemplateTags(template);
+    }
+    
+    blockReplace(name, data, template)
+    {
+        let blockTemplate = this.getBlockTemplate(name, template);
+        let blockMaked = blockTemplate;
+        
+        // remove block
+        if (data === false) {
+            return template.replace(blockMaked, "");
+        }
+        
+        const isMultiple = data instanceof Array && data[0] !== undefined;
+        
+        // prepare
+        if (isMultiple) {
+            blockMaked = "";
+            for (let key in data) {
+                let value = data[key];
+                if (typeof(value) == "object") {
+                    blockMaked += this.blockReplace(name, value, blockTemplate);
+                }
+            }
+        } else {
+            for (let key in data) {
+                let value = data[key];
+                // prepare sub block
+                if (typeof(value) == "object" || value === false) {
+                    blockMaked = this._blockReplace(key, value, blockMaked);
+                } else { // replace mask on value
+                    blockMaked = this.replaceAll('<!-- v[' + key + '] -->', value, blockMaked);
+                }
+            }
+        }
+        
+        // replace block
+        template = this.replaceAll(blockTemplate, blockMaked, template);
+        
+        return template;
+    }
 
-	skipTemplateTags(content) {
-		const regexp = /<!--(\s?\}|)\s?[bv]\[[^\]]+\]\s?(\{\s?|)-->/;
-		return content.replace(new RegExp(regexp,"gms"),"");
-	}
+    skipTemplateTags(content)
+    {
+        const regexp = /<!--(\s\}|)\s[bv]\[[^\]]+\]\s(\{\s|)-->/;
+        return content.replace(new RegExp(regexp, "gms"), "");
+    }
 
-	replaceAll(find,repl,str) {
-		const escapeRegExp = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-		return str.replace(new RegExp(escapeRegExp(find),'g'),repl);
-	}
+    replaceAll(find, repl, str)
+    {
+        const escapeRegExp = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        return str.replace(new RegExp(escapeRegExp(find), 'g'), repl);
+    }
 }
--- template.php
+++ template.php
@@ -1,84 +1,90 @@
 <?php
-namespace alvazar;
+namespace PAVApp\Core;
 
-class Template {
-	public function __construct() {
-	}
-	
-	protected function _getBlockTemplate($name,$template) {
-		$regexp = "/(\<\!\-\-\s?(b\[{$name}\])\s?\{\s?\-\-\>.+\<\!\-\-\s?\}\s?\g{2}\s?\-\-\>)/us";
-		preg_match_all($regexp,$template,$match);
-		return !empty($match[1][0]) ? $match[1][0] : "";
-	}
-	
-	public function make($path,$data,$onlyBlock="") {
-		$template = file_get_contents($path);
-		return $this->makeTemplate($template,$data,$onlyBlock);
-	}
+class Template
+{
+    public function __construct()
+    {
+    }
+    
+    protected function getBlockTemplate(string $name, string $template): string
+    {
+        $regexp = "/(\<\!\-\-\s(b\[{$name}\])\s\{\s\-\-\>.+?\<\!\-\-\s\}\s\g{2}\s\-\-\>)/us";
+        preg_match_all($regexp, $template, $match);
+        return !empty($match[1][0]) ? $match[1][0] : "";
+    }
+    
+    public function make(string $path, array $data, string $onlyBlock = ""): string
+    {
+        $template = file_get_contents($path);
+        return $this->makeTemplate($template, $data, $onlyBlock);
+    }
 
-	public function makeTemplate($template,$data,$onlyBlock="") {
-		if ($onlyBlock !== "") {
-			$template = $this->_getBlockTemplate($onlyBlock,$template);
-		}
-		foreach ($data as $name => $value) {
-			$template = $this->_blockReplace($name,$value,$template);
-		}
-		return $template;
-	}
-	
-	protected function _blockReplace($name,$data,$template) {
-		$blockTemplate = $this->_getBlockTemplate($name,$template);
-		$blockMaked = $blockTemplate;
-		
-		// remove block
-		if ($data === false) {
-			return str_replace($blockMaked,"",$template);
-		}
-		
-		$isMultiple = is_array($data) && isset($data[0]);
-		
-		// prepare
-		if ($isMultiple) {
-			$blockMaked = "";
-			foreach ($data as $value) {
-				if (is_array($value)) {
-					$blockMaked .= $this->_blockReplace($name,$value,$blockTemplate);
-				}
-			}
-		}
-		else {
-			foreach ($data as $key => $value) {
-				// prepare sub block
-				if (is_array($value) || $value === false) {
-					$blockMaked = $this->_blockReplace($key,$value,$blockMaked);
-				}
-				// replace mask on value
-				else {
-					$flag = "";
-					if (strpos($key,":") !== false) {
-						list($key,$flag) = explode(":",$key,1);
-					}
-					$value = $this->prepareValue($value,$flag);
-					$blockMaked = str_replace('<!-- v['.$key.'] -->',$value,$blockMaked);
-				}
-			}
-		}
-		
-		// replace block
-		$template = str_replace($blockTemplate,$blockMaked,$template);
-		
-		return $template;
-	}
+    public function makeTemplate(string $template, array $data, string $onlyBlock = ""): string
+    {
+        if ($onlyBlock !== "") {
+            $template = $this->getBlockTemplate($onlyBlock, $template);
+        }
+        foreach ($data as $name => $value) {
+            $template = $this->blockReplace($name, $value, $template);
+        }
+        return $template;
+    }
+    
+    protected function blockReplace(string $name, array $data, string $template): string
+    {
+        $blockTemplate = $this->getBlockTemplate($name, $template);
+        $blockMaked = $blockTemplate;
+        
+        // remove block
+        if ($data === false) {
+            return str_replace($blockMaked, "", $template);
+        }
+        
+        $isMultiple = is_array($data) && isset($data[0]);
+        
+        // prepare
+        if ($isMultiple) {
+            $blockMaked = "";
+            foreach ($data as $value) {
+                if (is_array($value)) {
+                    $blockMaked .= $this->blockReplace($name, $value, $blockTemplate);
+                }
+            }
+        } else {
+            foreach ($data as $key => $value) {
+                if (is_array($value) || $value === false) {
+                    // prepare sub block
+                    $blockMaked = $this->blockReplace($key, $value, $blockMaked);
+                } else {
+                    // replace mask on value
+                    $flag = "";
+                    if (strpos($key,":") !== false) {
+                        list($key, $flag) = explode(":", $key, 2);
+                    }
+                    $value = $this->prepareValue($value, $flag);
+                    $blockMaked = str_replace('<!-- v['.$key.'] -->', $value, $blockMaked);
+                }
+            }
+        }
+        
+        // replace block
+        $template = str_replace($blockTemplate, $blockMaked, $template);
+        
+        return $template;
+    }
 
-	public function skipTemplateTags($content) {
-		$regexp = "/\<\!\-\-(\s?\}|)\s?[bv]\[[^\]]+\]\s?(\{\s?|)\-\-\>/us";
-		return preg_replace($regexp,"",$content);
-	}
+    public function skipTemplateTags(string $content): string
+    {
+        $regexp = "/\<\!\-\-(\s\}|)\s[bv]\[[^\]]+\]\s(\{\s|)\-\-\>/us";
+        return preg_replace($regexp, "", $content);
+    }
 
-	public function prepareValue($value,$flag) {
-		if ($flag !== "raw") {
-			$value = htmlspecialchars($value);
-		}
-		return $value;
-	}
+    public function prepareValue(string $value, string $flag): string
+    {
+        if ($flag !== "raw") {
+            $value = htmlspecialchars($value);
+        }
+        return $value;
+    }
 }
